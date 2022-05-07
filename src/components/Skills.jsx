9844d604410834786b030612efd3d6b0e74385b6
Ivan Vidic <ividic010@gmail.com>
2022-05-07 20:59:02 +0200

    Initial Commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,59 @@
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import JavaScript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Git from "../assets/git.jpg";
+import TypeScript from "../assets/typescript.png";
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-[#D63B3B] ">
+            Skills
+          </p>
+          <p className="py-4">// These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+            <p className="my-4">CSS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="JAVASCRIPT icon"
+            />
+            <p className="my-4">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="REACT icon" />
+            <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-36 mx-auto" src={Git} alt="GIT icon" />
+            <p className="my-4">GIT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img
+              className="w-28 mx-auto"
+              src={TypeScript}
+              alt="TYPESCRIPT icon"
+            />
+            <p className="my-4">TYPESCRIPT</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
